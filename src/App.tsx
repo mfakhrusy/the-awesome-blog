f1b424caf83a5d9c38366c95094d50e362b51dc8
Muhamad Fakhrusy <fakhrusy.m@gmail.com>
2020-06-28 03:27:36 +0700

    test change

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App({}: AppProps) {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to wow
+          Edit <code>src/App.tsx</code> and save to wowaaa
         </p>
         <a
           className="App-link"
